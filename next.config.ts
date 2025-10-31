aadfa82f1798ace47b12b50b68d67a365bc681dc
Alvin Dennis <alvindennis80@gmail.com>
2025-10-31 12:58:50 +0530

    feat: remove cache from config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   compress: true,
-  cacheComponents: true
+  reactStrictMode: true,
 };
 
 export default nextConfig;
